1d425ea80b9145cc9467854236fc4003eaee20ff
John Fox <J.Arcoudis@gmail.com>
2016-04-06 20:58:21 +1000

    Lab End.
    Bug: Not Updating Data.
    Need to learn how to use Sessions

---
+++ Lab-3/Lab-3/GuessGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Lab_3
+{
+    public class GuessGame
+    {
+
+        private int answer;
+        private string previousGuesses;
+            
+        public string PreviousGuesses { get; }
+
+        public GuessGame()
+        {
+            ResetGame();
+        }
+
+        public void ResetGame()
+        {
+            Random r = new Random();
+
+            answer = r.Next(1, 10);
+        }
+
+        public bool MakeGuess(int guesse)
+        {
+            if(guesse == answer)
+            {
+                return true;
+            }
+
+            previousGuesses += " " + guesse;
+
+            return false;
+        }
+
+
+
+    }
+}
--- Lab-3/Lab-3/NumberGuess.aspx.cs
+++ Lab-3/Lab-3/NumberGuess.aspx.cs
@@ -9,19 +9,44 @@ namespace Lab_3
 {
     public partial class NumberGuesse : System.Web.UI.Page
     {
+        GuessGame game; 
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            game = (GuessGame)Session["Game"];
 
+            if( game == null)
+            {
+                game = new GuessGame();
+                game.ResetGame();
+                Session["Game"] = game;
+            }
+
+            lbl_prevGuess.Text = game.PreviousGuesses;
         }
 
         protected void btn_submitGuess_Click(object sender, EventArgs e)
         {
+            string guesse = txt_guess.Text;
+            int num;
+            bool isValid = int.TryParse(guesse, out num);
+            if (isValid)
+            {
+               bool isRight = game.MakeGuess(num);
+                    if(isRight)
+                {
+                    lbl_prevGuess.Text = "YOU WIN!";
+                }
+            }
 
+            
         }
 
         protected void btn_reset_Click(object sender, EventArgs e)
         {
-
+            game.ResetGame();
         }
+        
     }
+
 }
