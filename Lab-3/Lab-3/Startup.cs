72e0099501254719b49917c85b9bffd9e211324f
John Fox <J.Arcoudis@gmail.com>
2016-04-06 19:18:50 +1000

    Initial Commit

---
+++ Lab-3/Lab-3/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab_3.Startup))]
+namespace Lab_3
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
