91454f278e7cfa6a460e60a3076127348744b964
John Fox <J.Arcoudis@gmail.com>
2016-04-20 20:57:37 +1000

    end of class

---
+++ Lab5/Lab5/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Lab5
+{
+    public class Class1
+    {
+
+
+        public void getData()
+        {
+            using (SqlConnection c = new SqlConnection())
+            {
+
+                c.ConnectionString = ConfigurationManager.ConnectionStrings["Db"].ConnectionString;
+
+                string query = @"SELECT * FROM Foods";
+                SqlCommand command = new SqlCommand(query);
+                command.Connection = c;
+
+                command.ExecuteReader();
+
+            }
+
+
+
+        }
+       
+
+    }
+}
